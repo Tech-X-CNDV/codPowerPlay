2d457e18096957514362fdbdb0542f5476190e32
Tech X <techx.cndv2021@gmail.com>
2022-09-26 15:03:28 +0300

    mecanum

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOpMode.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+
+public class MecanumTeleOpMode extends OpMode {
+    private DcMotor leftFrontMotor = null;
+    private DcMotor rightFrontMotor = null;
+    private DcMotor leftRearMotor = null;
+    private DcMotor rightRearMotor = null;
+    private ElapsedTime runtime = new ElapsedTime();
+    private double leftStickForward = 0;
+    private double leftStickSide = 0;
+    private double botSpin = 0;
+    private double denominator = 0;
+    private double frontLeftPower = 0;
+    private double frontRightPower = 0;
+    private double rearLeftPower = 0;
+    private double rearRightPower = 0;
+
+    @Override
+    public void init(){
+        telemetry.addData("Status", "Initialized");
+        leftFrontMotor = hardwareMap.get(DcMotor.class, "leftFrontMotor");
+        rightFrontMotor = hardwareMap.get(DcMotor.class, "rightFrontMotor");
+        leftRearMotor = hardwareMap.get(DcMotor.class, "leftRearMotor");
+        rightRearMotor = hardwareMap.get(DcMotor.class, "rightRearMotor");
+
+        telemetry.update();
+    }
+
+    @Override
+    public void init_loop(){}
+
+    @Override
+    public void start(){runtime.reset();}
+
+    @Override
+    public void loop(){
+        leftStickForward = -this.gamepad1.left_stick_y;
+        leftStickSide = this.gamepad1.left_stick_x * 1.1;
+        botSpin = this.gamepad1.right_stick_x;
+
+        denominator = Math.max(Math.abs(leftStickForward) + Math.abs(leftStickSide) + Math.abs(botSpin), 1);
+        frontLeftPower = (leftStickForward + leftStickSide + botSpin) / denominator;
+        rearRightPower = (leftStickForward + leftStickSide - botSpin) / denominator;
+        frontRightPower = (leftStickForward - leftStickSide - botSpin) / denominator;
+        rearLeftPower = (leftStickForward - leftStickSide + botSpin) / denominator;
+
+        telemetry.addData("Left Stick Y", leftStickForward);
+        telemetry.addData("Left Stick X", leftStickSide);
+
+        leftFrontMotor.setPower(frontLeftPower);
+        rightRearMotor.setPower(rearRightPower);
+
+        rightFrontMotor.setPower(frontRightPower);
+        leftRearMotor.setPower(rearLeftPower);
+
+        telemetry.update();
+    }
+
+    @Override
+    public void stop(){}
+}
+
